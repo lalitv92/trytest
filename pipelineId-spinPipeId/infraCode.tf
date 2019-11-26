9a83daf5c0f28a0bad5ea8f8239b67cb3ef0a60c
OpsMx <Team@OpsMx.com>
2019-11-27 00:07:42 +0530

    g

---
+++ pipelineId-spinPipeId/infraCode.tf
@@ -0,0 +1,1 @@
+module "terraModule"{source = "git::https://lalitv92:Vikram92!@github.com/lalitv92/TerraformPlansModule.git//namespace"}
---
+++ pipelineId-spinPipeId/provider.tf
@@ -0,0 +1,1 @@
+provider "kubernetes"{ config_path = "/home/terraspin/opsmx/kubeaccount/my-k8s-v2-account" }
