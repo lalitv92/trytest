13cdb16c50f33b899a0ed05b4b7d6e5e8d0c914a
OpsMx <Team@OpsMx.com>
2019-12-02 03:40:02 +0000

    adding spinterra plan state

--- pipelineId-spinPipeId/infraCode.tf
+++ pipelineId-spinPipeId/infraCode.tf
@@ -1,1 +1,1 @@
-module "terraModule"{source = "git::https://lalitv92:56251619bdbb4fbf59e6ab0aa0aec8eba567b3a4@github.com/lalitv92/TerraformPlansModule.git//namespace"}
+module "terraModule"{source = "git::https://lalitv92:3141b3d3814a66298d8e1d8e0003f601130f092f@github.com/lalitv92/TerraformPlansModule.git//namespace"}
